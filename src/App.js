c3c3069aa40d5a73501c433a01e245a95b1f369e
Anupam Guin <anupamguin7585@gmail.com>
2021-11-16 23:03:26 +0530

    AddProject.js & CreateProjectButton.js, Dashboard.js & Header.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
