702135d869374bd291c72ec8a7b820298beaad1e
Anupam Guin <anupamguin7585@gmail.com>
2021-11-21 23:34:21 +0530

    Get Projects from backend and show it using map function in Dashboard.js

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
@@ -10,5 +10,17 @@ export const createProject = (project, history) => async (dispatch) => {
       type: GET_ERRORS,
       payload: error.response.data,
     });
+  }
+};
+
+export const getProjects = () => async (dispatch) => {
+  try {
+    const res = await axios.get("http://localhost:8082/api/project/all");
+    dispatch({
+      type: GET_PROJECTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.error(err);
   }
 };
